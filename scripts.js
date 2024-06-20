f6b50e7bb6019de8a691c81368315ccec526e280
AbraKadabraPuf <trai1s@yandex.ru>
2024-06-20 16:39:18 +0300

    favicon-32x32.png  scripts.js avatar-jessica.jpeg index.html styles.css

---
+++ scripts.js
@@ -0,0 +1,84 @@
+const hoverElement = document.querySelector('.link');
+const targetElement = document.querySelector('.')
+
+
+
+/*
+1. Сначала:
+мне унжно чтобы при наведении курсора на плашку она меняла цвет
+2. Потом:
+чтобы при нажатии на нее она переходила по определенной ссылке
+*/
+
+
+/*
+.Frontend-Mentor {
+	width: 300px;
+	height: 45px;
+	color: hsl(0, 0%, 100%);
+
+	background-color: hsl(0, 0%, 20%);
+	font-family: Inter, sans-serif;
+	font-weight: 600;
+	font-size: 14px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 8px;
+	margin-bottom: 15px;
+}
+
+.Linkedin {
+	width: 300px;
+	height: 45px;
+	color: hsl(0, 0%, 100%);
+	background-color: hsl(0, 0%, 20%);
+	font-family: Inter, sans-serif;
+	font-weight: 600;
+	font-size: 14px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 8px;
+	margin-bottom: 15px;
+}
+.VKontakte {
+	width: 300px;
+	height: 45px;
+	color: hsl(0, 0%, 100%);
+	background-color: hsl(0, 0%, 20%);
+	font-family: Inter, sans-serif;
+	font-weight: 600;
+	font-size: 14px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 8px;
+	margin-bottom: 15px;
+}
+.Instagram {
+	width: 300px;
+	height: 45px;
+	color: hsl(0, 0%, 100%);
+	background-color: hsl(0, 0%, 20%);
+	font-family: Inter, sans-serif;
+	font-weight: 600;
+	font-size: 14px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 8px;
+	margin-bottom: 15px;
+}
+/*### Primary
+- Green: hsl(75, 94%, 57%)
+- White: hsl(0, 0%, 100%)
+- Grey: hsl(0, 0%, 20%)
+- Dark Grey: hsl(0, 0%, 12%)
+- Off Black: hsl(0, 0%, 8%)
+- Font size (paragraph): 14px
+- Family: [Inter](https://fonts.google.com/specimen/Inter)
+- Weights: 400, 600, 700
+
+color: #fffb40;
+*/
